abd4e339d31a1d289723c05f530144eec4dab593
Arc <atandarokib@gmail.com>
2023-08-14 20:49:38 +0100

    up

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="py-32 w-full" id="hero">
       <div className="container flex flex-col justify-center gap-8 items-center">
-        <h1 className="max-w-[817px] font-bold bord text-center  rainbow-text text-5xl leading-[1.4] lg:text-7xl font-cartoonCookies lg:leading-[86.40px] tracking-wider">
+        <h1 className="max-w-[817px] break-all font-bold bord text-center  rainbow-text text-5xl leading-[1.4] lg:text-7xl font-cartoonCookies lg:leading-[86.40px] tracking-wider">
           COOOOOOOOOKIES
         </h1>
         <p className="max-w-[585px] text-center text-white text-2xl font-cartoonCookies leading-loose tracking-wide">
