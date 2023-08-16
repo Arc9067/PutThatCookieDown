e507748d931a364adc3ac620551ef50fb0ccf638
Arc <atandarokib@gmail.com>
2023-08-16 10:13:56 +0100

    up

--- src/Components/Gifs.jsx
+++ src/Components/Gifs.jsx
@@ -5,11 +5,11 @@ import cook1 from "../assets/cook1.png";
 
 const Gifs = () => {
   return (
-    <section className="py-24 w-full" id="gif">
+    <section className="py-24 w-full relative" id="gif">
+      <img src={cook1} alt="" className="w-40 absolute right-0 top-11 z-10 animate-pulse" />
       <div className="container grid  items-center justify-center gap-8">
         <div className="relative">
           <img src={vid3} alt="" className="w-full lg:w-[40rem]" />
-          <img src={cook1} alt="" className="w-40 absolute right-0 bottom-11" />
         </div>
       </div>
     </section>
